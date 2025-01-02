a91dd12e333748e2b4cd034a0af226c9ba4d0437
travmonkey <t.coding@batk.me>
2025-01-02 18:13:35 +0000

    Update palworld server to systemd service

--- modules/system/palworld-server.nix
+++ modules/system/palworld-server.nix
@@ -2,7 +2,7 @@
 
 {
   options = {
-    palworld-server.enable = lib.mkEnableOption "enable terraria server";
+    palworld-server.enable = lib.mkEnableOption "enable palworld server";
   };
 
   # variable to store home username for absolute paths
@@ -12,12 +12,15 @@
     systemd.services.palworld = {
       description = "Palworld Server";
       after = [ "network.target" ];
-      wants = [ "multi-user.target" ];
+      wantedBy = [ "multi-user.target" ];
 
       serviceConfig = {
-        WorkingDirectory = "/home/aphelios/.steam/steam/Steamapps/common/PalServer";
-        ExecStart = "${pkgs.bash}/bin/bash /home/aphelios/.steam/steam/Steamapps/common/PalServer/PalServer.sh";
+        WorkingDirectory =
+          "/home/aphelios/.steam/steam/Steamapps/common/PalServer";
+        ExecStart =
+          "${pkgs.bash}/bin/bash /home/aphelios/.steam/steam/Steamapps/common/PalServer/PalServer.sh";
         Restart = "always";
+        User = "aphelios";
       };
     };
   };
